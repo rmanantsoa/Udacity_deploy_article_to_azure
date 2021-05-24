43a3a09d9d5256d7f51cc29827b757f4e13dac79
Manantsoa <rtianamanantsoa@gmail.com>
2021-05-24 21:36:21 +0300

    update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'ENTER_STORAGE_ACCOUNT_NAME'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'storagemanantsoa'
     BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'ENTER_BLOB_STORAGE_KEY'
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'ENTER_IMAGES_CONTAINER_NAME'
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'https://storagemanantsoa.blob.core.windows.net/images/'
 
     SQL_SERVER = os.environ.get('SQL_SERVER') or 'manantsoadb.database.windows.net'
     SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'cmsdb'
